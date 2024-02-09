6191ddbc1c2677e12de254ac0d49edfd91dec3d8
Hazal Onler <hazaltaneri@gmail.com>
2024-02-09 18:25:37 -0500

    Localstorage was added to keep the current page mode

--- src/darkBackground.js
+++ src/darkBackground.js
@@ -1,14 +1,36 @@
 function myFunction() {
 
-    if  (element.style.backgroundColor === 'black') {
-        element.style.backgroundColor = 'white';
-        element.style.color = 'black';
+    element.classList.toggle('dark-mode')
+    if (element.classList.contains('dark-mode')) {
+        localStorage.setItem('darkMode', 'enable')
     } else {
-        element.style.backgroundColor = 'black';
-        element.style.color = 'white';
+        localStorage.setItem('darkMode', 'disable')
     }
 }
 
 let element  = document.body;
-let bodyElement = document.getElementById('darkSwitch');
-bodyElement.onclick = myFunction;
+let buttonElement = document.getElementById('darkSwitch');
+buttonElement.onclick = myFunction;
+
+if (localStorage.getItem('darkMode') === 'enable') {
+    element.classList.add('dark-mode') 
+}
+
+/*
+const buttons = document.querySelectorAll('.mode-toggle')
+
+buttons.forEach(button => {
+  button.addEventListener('click', function () {
+    document.body.classList.toggle('light-mode')
+
+    if (document.body.classList.contains('light-mode')) {
+      localStorage.setItem('lightMode', 'enabled')
+    } else {
+      localStorage.setItem('lightMode', 'disabled')
+    }
+  })
+})
+
+if (localStorage.getItem('lightMode') === 'enabled') {
+  document.body.classList.add('light-mode')
+} */
