7e3bdfe4a55b4ee403159579ac6fad8fe8198163
Hazal Onler <hazaltaneri@gmail.com>
2024-02-12 16:01:29 -0500

    Delete unnecessary code blocks

--- darkBackground.js
+++ darkBackground.js
@@ -14,22 +14,3 @@ buttonElement.onclick = myFunction;
 if (localStorage.getItem('darkMode') === 'enable') {
     element.classList.add('dark-mode') 
 }
-
-/*
-const buttons = document.querySelectorAll('.mode-toggle')
-
-buttons.forEach(button => {
-  button.addEventListener('click', function () {
-    document.body.classList.toggle('light-mode')
-
-    if (document.body.classList.contains('light-mode')) {
-      localStorage.setItem('lightMode', 'enabled')
-    } else {
-      localStorage.setItem('lightMode', 'disabled')
-    }
-  })
-})
-
-if (localStorage.getItem('lightMode') === 'enabled') {
-  document.body.classList.add('light-mode')
-} */
